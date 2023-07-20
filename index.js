b6aeeb56257bb380749a724bd0acf7fa4d22dfb1
Francis Asare <francecen@gmail.come>
2023-07-20 13:43:16 +0000

     

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
 function sleep(ms) {
-    return new Promise ((resolve) => setTimeout(resolve, ms));
-   }
-   const phrases = ["Graphic Designer","Video Editor"];
-   const el = document.getElementById("typing");
-  
-   let sleepTime = 70;
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+const phrases = ["Graphic Designer", "Video Editor"];
+const el = document.getElementById("typing");
 
-   let curPhraseIndex = 0;
-    
-   const writeLoop = async () => {
-    while (true){
-           let curWord = phrases[curPhraseIndex];
-             
-           for (let i = 0; i < curWord.length; i++){
-            el.innerText = curWord.substring(0,i + 1);
-            await sleep(sleepTime);
-          }
+let sleepTime = 50;
 
-           await sleep(sleepTime * 10);
+let curPhraseIndex = 0;
 
-           for (let i = curWord.length; i > 0; i--){
-            el.innerText = curWord.substring(0,i - 1);
-            await sleep(sleepTime);
-          }
+const writeLoop = async () => {
+  while (true) {
+    let curWord = phrases[curPhraseIndex];
 
-          await sleep(sleepTime * 5);
+    for (let i = 0; i < curWord.length; i++) {
+      el.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime);
+    }
 
-          if (curPhraseIndex === phrases.length - 1){
-            curPhraseIndex = 0;
-          } else {
-            curPhraseIndex++;
-          }
-          }
+    await sleep(sleepTime * 10);
 
-   };
+    for (let i = curWord.length; i > 0; i--) {
+      el.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime);
+    }
 
-   writeLoop();
+    await sleep(sleepTime * 5);
+
+    if (curPhraseIndex === phrases.length - 1) {
+      curPhraseIndex = 0;
+    } else {
+      curPhraseIndex++;
+    }
+  }
+};
+
+writeLoop();
